dd2cabed64d9ee21a17a0697697ffecaf9448f14
benaichalae <benaichalae@gmail.com>
2023-09-25 13:10:09 -0700

    p

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
+    except (ValueError, TypeError):
         sys.stderr.write("Exception: {}\n".format(e))
         return False
