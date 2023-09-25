ad4126968db9c2eb5b14a73627f6c2c846d62f4e
benaichalae <benaichalae@gmail.com>
2023-09-25 09:11:49 -0700

    p

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_count = 0
+    i = 0
+    try:
+        while i < x:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                printed_count += 1
+            i += 1
+    except IndexError:
+        pass
+    print()
+    return printed_count
