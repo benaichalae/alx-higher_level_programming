23c739bc1cf652990aca9512317675dfffe20dfe
benaichalae <benaichalae@gmail.com>
2023-10-02 12:31:03 -0700

    p

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
+    """Represent a rectangle."""
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """Initialize a new Rectangle.
+
+        Args:
+        width (int): The width of the new rectangle.
+        height (int): The height of the new rectangle.
+        """
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -18,6 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -29,19 +41,24 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
         return (0 if self.__width == 0 or self.__height == 0
                 else 2 * (self.__width + self.__height))
 
     def __str__(self):
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         rect_str = ""
         for _ in range(self.__height):
-            rect_str += "#" * self.__width + "\n"
-        return rect_str[:-1]
-
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+            rect_str += "#" * self.__width
+            if _ < self.__height - 1:
+                rect_str += "\n"
+        return rect_str
