8228c32e163faf0cc5ef43df55f1ec4114bb7fc8
benaichalae <benaichalae@gmail.com>
2023-10-09 18:55:24 -0700

    p

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""inherite from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+    def __init__(self, size):
+        """Intialize with size"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
