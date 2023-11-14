5dfec5d6b6c7996209f8c3921a2750738404fc3f
benaichalae <benaichalae@gmail.com>
2023-11-14 13:11:06 -0800

    SQL

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database table dump
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
