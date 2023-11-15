3eb316412e95e6c18022f9f173de69d2b15ed0ce
benaichalae <benaichalae@gmail.com>
2023-11-15 10:11:30 -0800

    SQL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
--- script that creates the database hbtn_0d_usa and the
--- table states on your MySQL server.
+-- script that creates the database hbtn_0d_usa and the table states on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	PRIMARY KEY(id),
