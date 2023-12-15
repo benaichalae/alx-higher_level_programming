bf6abae15f56eb901c5df89080d49ee2ec5b2a0c
benaichalae <benaichalae@gmail.com>
2023-12-15 06:06:48 -0800

    p

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     cursor = connection.cursor()
 
-    query = "SELECT * FROM states WHERE name \
+    query = "SELECT * FROM states WHERE name\
             LIKE 'N%' ORDER BY id ASC"
     cursor.execute(query)
     results = cursor.fetchall()
