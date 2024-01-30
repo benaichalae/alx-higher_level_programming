79f015d6dbbb3ed369c5aad21d894afcec44687a
benaichalae <benaichalae@gmail.com>
2024-01-30 13:01:31 -0800

    js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-if (!url || !filePath) {
-  console.error('Please provide both the URL and file path as arguments.');
-  process.exit(1);
-}
-request.get(url, (error, response, body) => {
+const request = require('request');
+const args = process.argv.slice(2);
+
+request.get(args[0], function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
+    console.log(error);
   } else {
-    // Write the response body to the specified file
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-      if (writeError) {
-        console.error('Error writing to file:', writeError);
-      } else {
-        console.log(`Content successfully written to ${filePath}`);
-      }
+    fs.writeFile(args[1], body, function (error, response) {
+      if (error) console.log('error', error);
     });
   }
 });
