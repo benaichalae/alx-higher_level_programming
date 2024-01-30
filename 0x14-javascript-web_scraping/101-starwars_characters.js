1f1164ee64bc33d01b8c9c1d2e11a5134639c644
benaichalae <benaichalae@gmail.com>
2024-01-30 13:49:14 -0800

    js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-if (process.argv.length !== 3) {
-  console.error('Usage: ./101-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
