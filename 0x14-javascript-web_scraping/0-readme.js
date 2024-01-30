31450b6889c35e324ad5999c08ee5b19ccc0deb6
benaichalae <benaichalae@gmail.com>
2024-01-30 12:23:33 -0800

    js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+if (!filePath) {
+  console.error('Please provide the file path as an argument.');
+  process.exit(1);
+}
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
