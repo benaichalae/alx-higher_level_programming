698a52f042f924561f7d9118fb0818b792125c25
benaichalae <benaichalae@gmail.com>
2024-01-30 12:47:29 -0800

    js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+
+
 if (!apiUrl) {
   console.error('Please provide the Star Wars API URL as an argument.');
   process.exit(1);
 }
+
+
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
