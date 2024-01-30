f4ac0f317712c070b38a62cc725c4fa66cc8f22c
benaichalae <benaichalae@gmail.com>
2024-01-30 13:08:04 -0800

    js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+if (process.argv.length !== 3) {
+  console.error('Usage: ./100-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+  } else {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error:', charError);
+        } else if (charResponse.statusCode !== 200) {
+          console.error('Invalid status code:', charResponse.statusCode);
+        } else {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
