75f30fcb2049b45846ecee86f46ef62560ad7b58
benaichalae <benaichalae@gmail.com>
2024-01-30 12:28:52 -0800

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
+const [filePath, contentToWrite] = process.argv.slice(2);
 if (!filePath || !contentToWrite) {
   console.error('Please provide both the file path and content as arguments.');
   process.exit(1);
