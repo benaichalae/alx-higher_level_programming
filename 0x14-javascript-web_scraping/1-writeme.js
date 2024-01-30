9f8ec8594646a2cb7086ebc39c39085b2fb46d91
benaichalae <benaichalae@gmail.com>
2024-01-30 12:33:51 -0800

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const [filePath, contentToWrite] = process.argv.slice(2);
-if (!filePath || !contentToWrite) {
-  console.error('Please provide both the file path and content as arguments.');
-  process.exit(1);
-}
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+const process = require('process');
+const args = process.argv.slice(2);
+fs.writeFile(args[0], args[1], (err) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
